80ef29232a0cb97695dc6d43463cd7c3107e5e97
Tatiana Lavrovskaya <lavr39820@gmail.com>
2020-05-15 16:25:47 +0300

    Добавлена отправка форм

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 'use strict';
 
 import togglePopUp from './modules/togglePopUp';
+import sendForm from './modules/sendForm.js';
+
 
 // PopUp по клику "Перезвоните мне"
 togglePopUp();
+
+// Отправка данных с форм
+sendForm();
---
+++ src/modules/sendForm.js
@@ -0,0 +1,97 @@
+const sendForm = () => {
+
+  const forms = document.querySelectorAll('form');
+
+  const errorMessage = 'Что-то пошло не так',
+        successMessage = 'Ваша заявка отправлена! Мы скоро с Вами свяжемся!',
+        spinner = document.querySelector('.loadingio-spinner-spinner-ivatvn3eux');
+
+  
+  
+  forms.forEach((form) => {
+
+    const inputs = form.querySelectorAll('input');
+    inputs.forEach((item) => {
+      console.log(item);
+      
+      if (item.className === 'phone-user') {
+        item.addEventListener('keypress', e => {
+          if(!/[\+\d]/.test(e.key)) {
+            e.preventDefault();
+          }
+        });
+      }
+      if (item.placeholder === 'Ваше имя') {
+        item.addEventListener('keypress', (e) => {
+          if(!/[А-Яа-яЁё\s]/.test(e.key)) {
+            e.preventDefault();
+          }
+        });
+      }
+
+    });
+
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const statusMessage = document.createElement('div');
+      statusMessage.style.cssText = 'font-size: 1.5rem; color: black;';
+      
+      form.appendChild(statusMessage);
+      statusMessage.appendChild(spinner);
+
+      const formData = new FormData(form);
+      let body = {};      
+      formData.forEach((val, key) => {
+        body[key] = val;
+      });
+
+      
+      postData(body)
+        .then((response) => {
+          if (response.status !== 200) {
+            throw new Error('Status network is not 200');
+          }
+          statusMessage.removeChild(spinner);
+          statusMessage.textContent = successMessage;
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 5000);
+        })
+        .catch((error) => {
+          statusMessage.removeChild(spinner);
+          statusMessage.textContent = errorMessage;
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 5000);
+          console.error(error);
+        });
+      
+      clearInputs(form);
+    });
+  });
+  
+
+  const postData = (body) => {
+    
+    return fetch('./server.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    });      
+  
+  };
+
+  const clearInputs = (elem) => {
+    let inputs = elem.querySelectorAll('input');
+    inputs.forEach((i) => {
+      i.value = '';
+    });
+  };
+
+
+
+};
+
+export default sendForm;
